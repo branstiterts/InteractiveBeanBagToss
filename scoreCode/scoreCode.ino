8fe6b78c55ded30c23c2eb7554ede31d9f9ab383
branstiterts <branstiterts@gmail.com>
2014-05-21 08:59:32 -0500

    Added some comments throughout code

--- scoreCode/scoreCode.ino
+++ scoreCode/scoreCode.ino
@@ -6,6 +6,7 @@ int clockPin = 5;
 
 int btnPin = 3;
 
+// Initialize our LED strip containing 36 pixels
 LPD6803 strip = LPD6803(36, dataPin, clockPin);
 
 volatile int circleStart = 31;
@@ -14,11 +15,13 @@ const long initialScoreTime = 5000;
 
 const int minimumDelay = 40;
 
+// Set current state of PIR sensor (scored) and current state of push button (btnChanged)
 volatile boolean scored = false;
 volatile boolean btnChanged = false;
 
 volatile int btnCount = 0;
 
+// Color definitions to be used later
 unsigned int red = Color(0, 0, 255);
 unsigned int green = Color(255, 0, 0);
 unsigned int blue = Color(0, 255, 0);
@@ -32,14 +35,20 @@ void setup() {
   strip.setCPUmax(90);  // start with 50% CPU usage. up this if the strand flickers or is slow
   strip.begin();
 
+  // Method created to animate lights while the PIR sensor is calibrating and let us know when done
   loadingLights();
   Serial.println("done loading");
+  
+  // Interrupt 0 on Pin 2 for PIR Sensor and Interrupt 1 on Pin 3 for push button
   attachInterrupt(0, interruptPir, RISING);
   attachInterrupt(1, interruptBtn, RISING);
 }
 
 void loop() {
   Serial.println("start of loop()");
+  // Pushing button changes color of the lights, default is our rainbow cycle
+  // Single push of button will change value of btnCount to 1 which changes color to Red
+  // Seeing interruptBtn()
   if(btnCount == 0) {
     rainbow(50);
   } else if (btnCount == 1) {
@@ -51,9 +60,14 @@ void loop() {
   } else if (btnCount == 4) {
     solidColor(pink);
   }
+  
+  // If button is pushed, btnChanged value becomes true, reset btnChanged back to false
   if(btnChanged) {
     btnChanged = false;
   }
+  
+  // If the interruptPir() is called, value of scored becomes true, run the score() method to animate lights
+  // Reset scored back to false
   if(scored) {
     score();
     scored = false;
@@ -61,7 +75,10 @@ void loop() {
   Serial.println("end of loop()");
 }
 
+// LED Animation for if PIR is triggered (i.e. someone has scored)
 void score() {
+  // Figure out which animation to run depending on color on the board (btnCount), if rainbow run flashy animation
+  // If btnCount is greater than 0 (board color is not flashy just a solid color) so run that animation
   if(btnCount == 0) {
    scoreNormal(); 
   } else if (btnCount > 0) {
@@ -70,8 +87,10 @@ void score() {
   scored = false;
 }
 
+// LED animation for when color is on rainbow cycle, very flashy and cool
 void scoreNormal() {
  int i;
+ // Set outside LED strip to every other pixel being blue and the others are off
   for(i = 0; i < circleStart; i++) {
     if(i % 2 == 0) {
       strip.setPixelColor(i, Color(0,255,0)); 
@@ -94,6 +113,9 @@ void scoreNormal() {
 //    }
 //    strip.show();
 //    delay(100);
+    
+    // LED animation for inner LED strip (the circle)
+    // LED chase, one pixel at a time, changing colors each time around (rainbow effect)
     for(i = circleStart; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(color % 96));
       strip.show();
@@ -101,16 +123,18 @@ void scoreNormal() {
 //      strip.setPixelColor(i, Color(0,0,0));
 //      strip.show();
 //      delay(100);
-      color += 5;
+      color += 5;  // Change this value to a lower number to get more colors, harder to see color change as it is so little though
     }
-     for(i = circleStart; i < strip.numPixels(); i++) {
+    
+    // Turn all pixels to black (or off) when animation is done
+    for(i = circleStart; i < strip.numPixels(); i++) {
 //      strip.setPixelColor(i, Wheel(color % 96));
 //      strip.show();
 //      delay(100);
       strip.setPixelColor(i, Color(0,0,0));
       strip.show();
       delay(100);
-      color += 5;
+      color += 5;  // May not need this line
     }
   }
   
@@ -119,6 +143,9 @@ void scoreNormal() {
   }
   strip.show();
   
+  // Finish scoring animation by creating a lock in effect
+  // Starts at top of the board in the middle and seperates a pixel at a time
+  // One to the right, one to the left and finshes by meeting at the bottom in the middle
   int outerMiddle = circleStart / 2;
   int nextLeds = 0;
   for(i = outerMiddle; i < circleStart; i++) {
@@ -130,14 +157,17 @@ void scoreNormal() {
   } 
 }
 
+// LED animation for if the board color is a solid color (i.e. red, green, etc.), plain and simple animation
 void scoreSolidColor(unsigned int color) {
   int i, j;
+  // Wipe from start of circle, clockwise to fill circle with white
   for(i = circleStart; i < strip.numPixels(); i++) {
    strip.setPixelColor(i, Color(255,255,255)); 
    strip.show();
    delay(100);
   }
   delay(10000);
+  // Wipe from end of circle, counter-clockwise to fill circle with original color as before
   for(j = strip.numPixels(); j >= circleStart; j--) {
     strip.setPixelColor(j, color);
     strip.show();
@@ -145,10 +175,12 @@ void scoreSolidColor(unsigned int color) {
   }
 }
 
+// If PIR Sensor detects movement this interrupt is called
 void interruptPir() {
  scored = true; 
 }
 
+// If push button is pressed, this interrupt is called
 void interruptBtn() {
   if(!btnChanged) {
     Serial.println("In the interrupt");
@@ -157,10 +189,12 @@ void interruptBtn() {
   }
 }
 
+// Loading animation to wait for PIR sensor to be fully calibrated
 void loadingLights() {
   int i;
   unsigned long start = millis();
-  while(millis() - start < 10000) {
+  while(millis() - start < 10000) {  // Run this for 1 minute
+    // LED animation to do a red pixel chase, 3 pixels at a time
     for(i = circleStart - 3; i >= 0; i -= 3) {
        int a = millis();
        strip.setPixelColor(i, Color(0,0,255));
@@ -175,6 +209,8 @@ void loadingLights() {
        strip.show();
     }
   }
+  
+  // Once the minute is up, let the players know by lighting outside of board as green
   for(i = circleStart - 1; i >= 0; i--){
     strip.setPixelColor(i, Color(255,0,0));
     //delay(30); 
@@ -184,20 +220,22 @@ void loadingLights() {
   delay(2500);
 }
 
+// Rainbow cycle LED animation
 void rainbow(uint8_t wait) {
   int i, j = 0;
   
-  while(j < 96 * 5 && !scored && !btnChanged) {
-//  for (j=0; j < 96 * 5; j++) {     // 5 cycles of all 96 colors in the wheel
+  while(j < 96 * 5 && !scored && !btnChanged) {  // 5 cycles of all 96 colors in the wheel
+//  for (j=0; j < 96 * 5; j++) {    
     for (i=0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel( ((i * 96 / strip.numPixels()) + j) % 96) );
     }
-    strip.show();   // write all the pixels out
+    strip.show(); 
     delay(wait);
     j++;
   }
 }
 
+// Sets board to specific solid color depending on value of btnCount
 void solidColor(unsigned int color) {
   int i = 0;
   while(i < strip.numPixels() && !scored && !btnChanged) {
